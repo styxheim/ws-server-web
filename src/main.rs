233005c984b877863e7424f7075809b6e1cab502
Noktoborus <noktoborus@gmail.com>
2021-01-20 00:05:03 +0300

    example for cookie set/get

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,40 @@ extern crate serde_derive;
 #[macro_use]
 extern crate rocket;
 extern crate rocket_contrib;
+use rocket::http::Cookie;
+use rocket::http::Cookies;
 
 use rocket_contrib::templates::Template;
 
 #[derive(Serialize)]
 struct TemplateContext {
-    title: &'static str,
-    parent: &'static str,
+  title: String,
+  parent: &'static str,
 }
 
 #[get("/")]
-fn index() -> Template {
-    Template::render(
-        "index",
-        &TemplateContext {
-            title: "Hello",
-            parent: "layout",
-        },
-    )
+fn index(mut cookies: Cookies) -> Template {
+  let cookie = Cookie::build("int", "val").finish();
+  let intval = cookies
+    .get("int")
+    .unwrap_or(&Cookie::build("int", "N-O-N-E").finish())
+    .value()
+    .to_string();
+
+  cookies.add(cookie);
+
+  Template::render(
+    "index",
+    &TemplateContext {
+      title: intval,
+      parent: "layout",
+    },
+  )
 }
 
 fn main() {
-    rocket::ignite() 
-        .mount("/", routes![index])
-        .attach(Template::fairing())
-        .launch();
+  rocket::ignite()
+    .mount("/", routes![index])
+    .attach(Template::fairing())
+    .launch();
 }
